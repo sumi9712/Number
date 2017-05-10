c74eff325d9a35d474ba0e3541258aad23a86bc7
Suman <sumi9712@gmail.com>
2017-05-10 12:17:31 -0700

    initial commit

---
+++ src/NumberApp.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class NumberApp {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome!!");
+		int i =0;
+		String Numberinwords = "";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		
+				
+		i = sc.nextInt();
+		
+	switch ( i)
+	{
+		case 1:
+			 Numberinwords = "one";
+			break;
+			
+		case 2:
+			Numberinwords = "Two";
+			break;
+			
+		case 3:
+			Numberinwords = "Three";
+			break;
+			
+		case 4:
+			Numberinwords = "Four";
+			break;
+			
+		case 5:
+			Numberinwords = "Five";
+			break;
+			
+		case 6:
+			Numberinwords = "Six";
+			break;
+			
+		case 7:
+			Numberinwords = "Seven";
+			break;
+			
+		case 8:
+			Numberinwords = "Eight";
+			break;
+			
+		case 9:
+			Numberinwords = "Nine";
+			break;
+			
+		case 10:
+			Numberinwords = "Ten";
+			break;
+			
+		
+		default:
+		Numberinwords = "number not found";
+		break;
+	}
+		
+		System.out.println("Number in words: " + Numberinwords);
+	
+	}
+
+	
+}
